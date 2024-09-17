fc33afd7c9daf329fe766f508ffa768b18029ba4
Sumit-Samadhiya <sumitsamadhiya877@gmail.com>
2024-09-17 22:28:17 +0530

    Update

---
+++ fireBase/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='h-[60px] flex justify-center items-center bg-white m-4 rounded-lg'>
+        
+            <img src='/logos_firebase.svg' />
+            <h1>Firebase Contact App</h1>
+      
+    </div>
+  )
+}
+
+export default Navbar
